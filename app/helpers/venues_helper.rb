bddbc1334839a2330932f84fe988776fcba11e5e
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-01-13 18:46:38 +0100

    Changed venue questions style

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -39,4 +39,20 @@ module VenuesHelper
         "( %s in %s Reviews )" % [ venue.avg_venue_rating, venue.reviews_count ]
       end
    end
+
+  def comment_avatar(user)
+    if user.avatar.attached?
+      image_tag(user.avatar.variant(resize_and_pad: [ 500, 500 ]))
+    else
+      image_tag("default_avatar.png")
+    end
+  end
+
+  def review_avatar(user)
+    if user.avatar.attached?
+      image_tag(user.avatar.variant(resize_and_pad: [ 500, 500 ]))
+    else
+      image_tag("default_avatar.png")
+    end
+  end
 end
