147551509fa0462bc07ad4a53bb5d966a7a4aeb7
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-01-30 13:36:38 +0100

    Rubocop autolint

--- app/abilities/answer_ability.rb
+++ app/abilities/answer_ability.rb
@@ -2,24 +2,23 @@
 
 class AnswerAbility
     include CanCan::Ability
-  
+
     def initialize(user)
         can :read, Answer
-  
+
         return unless user.present?
-  
-  
+
+
         if user.reviewer?
           can :manage, Answer, user_id: user.id
         end
-  
+
         if user.owner?
           can :manage, Answer, user_id: user.id
         end
-  
+
         if user.admin? || user.moderator?
           can :manage, Answer
         end
       end
-  end
-  
+end
